db7052efaf8aed115d9cefa1d4850cda783b7a29
leekeh <liekebeunders@gmail.com>
2023-03-19 15:09:30 +0100

    Add simple map with openlayers and open street map

--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -1,1 +1,37 @@
-<h1>This will be a map</h1>
+<script lang="ts">
+	import Map from 'ol/Map.js';
+	import OSM from 'ol/source/OSM.js';
+	import TileLayer from 'ol/layer/Tile.js';
+	import View from 'ol/View.js';
+	import { onMount } from 'svelte';
+
+	let map: Map;
+
+	onMount(async () => {
+		map = new Map({
+			layers: [
+				new TileLayer({
+					source: new OSM()
+				})
+			],
+			target: 'map',
+			view: new View({
+				center: [0, 0],
+				zoom: 2
+			})
+		});
+	});
+</script>
+
+<svelte:head>
+	<link rel="stylesheet" href="node_modules/ol/ol.css" />
+</svelte:head>
+
+<div id="map" class="map" draggable="false" />
+
+<style>
+	.map {
+		width: 100vw;
+		height: 100vh;
+	}
+</style>

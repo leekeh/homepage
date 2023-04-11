b574a693f27be1ad56fe0a4f94b85d62fc80e2a4
leekeh <liekebeunders@gmail.com>
2023-04-11 11:10:05 +0200

    Hide sidebar on mobile and add responsive margins

--- src/routes/(homepage)/tutorial/+page.svelte
+++ src/routes/(homepage)/tutorial/+page.svelte
@@ -4,21 +4,14 @@
 </script>
 
 <div>
-	<aside>sidebar</aside>
-
-	<main>
-		main itnrodlc
-
-		<br />
-		is Desktop: {$isDesktop}
-		<br />
-		is Tablet: {$isTablet}
-		<br />
-		isLargeScreen: {$isLargeScreen}
-	</main>
-
-	<IconButton href="/" alt="tem[" style="position: absolute; right: 4px; top: 4px;">test</IconButton
-	>
+	{#if $isDesktop}
+		<aside>sidebar</aside>
+	{:else}
+		<IconButton href="/" alt="tem[" style="position: absolute; right: 4px; top: 4px;"
+			>test</IconButton
+		>
+	{/if}
+	<main>main itnrodlc</main>
 </div>
 
 <style>
@@ -26,17 +19,32 @@
 		border-right: 2px solid var(--accent);
 	}
 	div {
+		--spacing: 10px;
 		position: relative;
-		--spacing: 40px;
 		outline: 2px solid var(--accent);
 		width: calc(100vw - var(--spacing) * 2);
 		height: calc(100vh - var(--spacing) * 2);
-		margin: var(--spacing);
+		margin: calc(var(--spacing) + env(safe-area-inset-left))
+			calc(var(--spacing) + env(safe-area-inset-right))
+			calc(var(--spacing) + env(safe-area-inset-bottom))
+			calc(var(--spacing) + env(safe-area-inset-left));
 		background: var(--bg-primary);
 		border-radius: 30px;
 		display: grid;
 		grid-template-columns: 1fr 4fr;
 
 		align-items: stretch;
+	}
+
+	@media (min-width: 768px) {
+		div {
+			--spacing: 20px;
+		}
+	}
+
+	@media (min-width: 1024px) {
+		div {
+			--spacing: 40px;
+		}
 	}
 </style>

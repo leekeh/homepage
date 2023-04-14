03142f72d5875a48a54dda1e824773b6a83c9522
leekeh <liekebeunders@gmail.com>
2023-04-15 00:07:55 +0200

    Add outline animation on hover to iconbutton

--- src/components/IconButton.svelte
+++ src/components/IconButton.svelte
@@ -4,7 +4,9 @@
 	export let style: string = '';
 </script>
 
-<a {href} aria-label={alt} target="_blank" {style}><slot /></a>
+<a {href} aria-label={alt} target="_blank" {style}>
+	<slot />
+</a>
 
 <style>
 	a {
@@ -12,20 +14,53 @@
 		height: 3rem;
 		color: var(--accent);
 		text-decoration: none;
-		border-radius: 100%;
 		padding: 0.5rem;
 		position: relative;
 		-webkit-tap-highlight-color: transparent;
+		border-radius: 100%;
+		outline: none;
 	}
 
-	a:hover,
-	a:focus-visible {
+	a::before {
+		position: absolute;
+		border-radius: 100%;
+		content: '';
+		top: 0;
+		left: 0;
+		bottom: 0;
+		right: 0;
+	}
+
+	@keyframes drawOutline {
+		0% {
+			clip-path: polygon(50% 0%, 50% 0, 50% 0, 50% 0, 50% 0, 50% 0, 50% 50%);
+		}
+		12.5% {
+			clip-path: polygon(50% 0%, 100% 0, 100% 0, 100% 0, 100% 0, 100% 0, 50% 50%);
+		}
+		37.5% {
+			clip-path: polygon(50% 0%, 100% 0, 100% 100%, 100% 100%, 100% 100%, 100% 100%, 50% 50%);
+		}
+		62.5% {
+			clip-path: polygon(50% 0%, 100% 0, 100% 100%, 0 100%, 0 100%, 0 100%, 50% 50%);
+		}
+		87.5% {
+			clip-path: polygon(50% 0%, 100% 0, 100% 100%, 0 100%, 0 0, 0 0, 50% 50%);
+		}
+		100% {
+			clip-path: polygon(50% 0%, 100% 0, 100% 100%, 0 100%, 0 0, 50% 0, 50% 50%);
+		}
+	}
+
+	a:hover::before,
+	a:focus-visible::before {
 		outline: var(--border-width) solid var(--accent);
 		outline-offset: calc(var(--border-width) * -1);
 	}
-
-	a:active {
-		background: var(--accent);
-		color: var(--bg-primary);
+	@media (prefers-reduced-motion: no-preference) {
+		a:hover::before,
+		a:focus-visible::before {
+			animation: drawOutline 0.25s linear;
+		}
 	}
 </style>

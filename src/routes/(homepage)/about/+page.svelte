44828394b8848ecc8dff17a5dfbada03e85bfe91
leekeh <71131586+leekeh@users.noreply.github.com>
2024-03-15 14:27:59 +0100

    add simple about page

--- src/routes/(homepage)/about/+page.svelte
+++ src/routes/(homepage)/about/+page.svelte
@@ -1,8 +1,14 @@
-<p>
-	quick about this website <br />
-	sources of freebies bg image:
-	<a
-		href="https://www.freepik.com/free-vector/mostera-background_4258347.htm#query=plant%20pattern&position=11&from_view=keyword&track=ais"
-		>Image by rawpixel.com</a
-	> on Freepik https://tablericons.com/ for the icons
-</p>
+<script lang="ts">
+	import { Card, Heading, Link } from '@components';
+</script>
+
+<Card style="max-width: var(--max-width)">
+	<Heading as="h1">About this page</Heading>
+	<p>
+		This website is a place for me to document my projects and play around with code. The source
+		code can be found on <Link href="https://github.com/leekeh/homepage">GitHub</Link>. The
+		background image comes from <Link
+			href="https://www.freepik.com/free-vector/mostera-background_4258347.htm">Rawpixel</Link
+		> and the icons are from <Link href="https://tablericons.com/">Tabler Icons</Link>.
+	</p>
+</Card>

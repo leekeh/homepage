343bd2e60cdb719f11986ece1b93e6daa2b41651
leekeh <71131586+leekeh@users.noreply.github.com>
2026-06-22 14:36:29 +0200

    fix: assets bug

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,23 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  compilerOptions: {
-    experimental: {
-      async: true,
-    },
-  },
-  kit: {
-    adapter: adapter({
-      fallback: "404.html",
-    }),
-    alias: {
-      "@components/*": "src/components/*",
-      "@icons/*": "src/icons/*",
-      "@widgets/*": "src/components/widgets/*",
-      "@assets/*": "Src/assets/*",
-    },
-  },
+	compilerOptions: {
+		experimental: {
+			async: true
+		}
+	},
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		alias: {
+			'@components/*': 'src/components/*',
+			'@icons/*': 'src/icons/*',
+			'@widgets/*': 'src/components/widgets/*',
+			'@assets/*': 'src/assets/*'
+		}
+	}
 };
 
 export default config;

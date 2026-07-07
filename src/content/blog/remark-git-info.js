cba23dc47cc4ddd5ee9789b28846e16302fb5096
leekeh <71131586+leekeh@users.noreply.github.com>
2026-07-07 11:22:36 +0200

    debugging logs

--- src/content/blog/remark-git-info.js
+++ src/content/blog/remark-git-info.js
@@ -39,7 +39,8 @@ function getGitHistory(filePath) {
 					message: line.slice(idx + 1)
 				};
 			});
-	} catch {
+	} catch (e) {
+		console.log('failed git stuff', e);
 		return [];
 	}
 }
@@ -60,6 +61,7 @@ export function remarkGitInfo() {
 		const lastModified = history.length > 0 ? history[0].date : null;
 		const publishedAt = history.length > 0 ? history[history.length - 1].date : null;
 		const changelog = history.map((e) => e.message);
+		console.log({ lastModified, publishedAt, changelog, history, filePath });
 
 		if (!file.data.fm || typeof file.data.fm !== 'object') {
 			file.data.fm = {};

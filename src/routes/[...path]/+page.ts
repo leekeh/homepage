19b9421d70805f6eb3060d673646bc3c960a45f8
leekeh <71131586+leekeh@users.noreply.github.com>
2026-09-01 23:07:07 +0200

    fix: build errors

--- src/routes/[...path]/+page.ts
+++ src/routes/[...path]/+page.ts
@@ -6,5 +6,6 @@ export function entries() {
 	return widgetNavigationData
 		.map((widget) => widget.route)
 		.filter((route) => route !== '/' && !route.includes('['))
+		.filter((route) => route !== '/blog')
 		.map((fullPath) => ({ path: fullPath.replace(/^\//, '') }));
 }

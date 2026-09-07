d6e5269c1b80efe716b577a00e511abfde0dc391
leekeh <71131586+leekeh@users.noreply.github.com>
2026-09-07 13:50:48 +0200

    fix: clean up rss xsl

--- src/content/rss.ts
+++ src/content/rss.ts
@@ -39,7 +39,6 @@ ${categories}
 		.join('\n');
 
 	return `<?xml version="1.0" encoding="UTF-8"?>
-<?xml-stylesheet type="text/xsl" href="/xsl/rss.xsl"?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 <channel>
 <title>${escapeXml(options.title)}</title>
